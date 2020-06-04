4603963e07deeb1ea005474e0ba11321255b0428
Melvin <melvincarvalho@gmail.com>
2020-06-04 13:37:40 +0200

    add gallery

---
+++ gallery.js
@@ -0,0 +1,19 @@
+import 'https://unpkg.com/dataisland?module'
+
+var id = 'data'
+
+function displayImage (uri) {
+  var i = window.document.createElement('img')
+  i.src = uri
+  i.onclick = displayRandomImage
+  window.document.body.innerHTML = ''
+  window.document.body.appendChild(i)
+}
+
+function displayRandomImage () {
+  let random = Math.floor(Math.random() * (di[id].length - 1)) + 1
+  let randomImage = di[id][random]
+  displayImage(randomImage)
+}
+
+displayRandomImage()
