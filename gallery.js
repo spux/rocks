8c764c6988f8c67d2f3a83d1b4c0589841d2dc66
Melvin <melvincarvalho@gmail.com>
2020-06-06 20:11:53 +0200

    add unfluff type

--- gallery.js
+++ gallery.js
@@ -1,12 +1,18 @@
 import 'https://unpkg.com/dataisland?module'
 
+var arr
 var id = 'data'
-var arr = di[id]
+
+if (di[id].link) {
+  arr = di[id].links.filter(i => i).map(i => i.href)
+} else {
+  arr = di[id]
+}
 
 function displayImage (uri) {
   var i = window.document.createElement('img')
   i.src = uri
-  i.onclick = () => displayRandomImage
+  i.onclick = () => displayRandomImage(arr)
   i.height = Math.floor(window.innerHeight * 0.96)
   window.document.body.innerHTML = ''
   window.document.body.appendChild(i)
