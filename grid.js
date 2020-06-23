1d07b7f481a55a559b912cb89e1400efc868d8db
Melvin <melvincarvalho@gmail.com>
2020-06-23 13:21:24 +0200

    inline grid css

--- grid.js
+++ grid.js
@@ -3,6 +3,12 @@ import 'https://jspm.dev/dior'
 import Grid from 'https://jspm.dev/gridjs'
 
 document.getElementsByTagName('body')[0].setAttribute('id', 'wrapper')
+document
+  .getElementsByTagName('head')[0]
+  .insertAdjacentHTML(
+    'beforeend',
+    `<link rel="stylesheet" href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css" />`
+  )
 
 var columns = Object.keys([].concat(di.data)[0])
 var values = [].concat(di.data).map(Object.values)
