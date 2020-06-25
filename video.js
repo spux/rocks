8b7145a5d427c9828a433f1c11f76d8e09ea7146
Melvin <melvincarvalho@gmail.com>
2020-06-25 17:51:49 +0200

    add qs logic

--- video.js
+++ video.js
@@ -2,6 +2,25 @@ import 'https://jspm.dev/dior'
 import { h, html, render, Component } from 'https://unpkg.com/spux?module'
 import Plyr from 'https://jspm.dev/plyr'
 
+globalThis.qs = Object.fromEntries(
+  new URLSearchParams(document.location.search)
+)
+
+globalThis.defaults = {
+  provider: 'youtube',
+  embedid: 'm3jNb7IdJHQ'
+}
+
+var provider, embedid
+
+if (di && di.data) {
+  provider = di.data.provider
+  embedid = di.data.embedid
+}
+
+provider = qs.provider || provider || defaults.provider
+embedid = qs.embedid || embedid || defaults.embedid
+
 document.head.insertAdjacentHTML(
   'beforeend',
   `<link rel="stylesheet" href="https://unpkg.com/plyr@3/dist/plyr.css" />`
@@ -12,8 +31,8 @@ render(
     <div class="container">
       <div
         id="player"
-        data-plyr-provider="youtube"
-        data-plyr-embed-id="${di.data.youtubeid}"
+        data-plyr-provider="${provider}"
+        data-plyr-embed-id="${embedid}"
       ></div>
     </div>
   `,
