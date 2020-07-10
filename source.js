3de15038d004a8550399b1aca13db40fb6c94042
Melvin <melvincarvalho@gmail.com>
2020-07-10 18:07:25 +0200

    change to source icon

--- source.js
+++ source.js
@@ -11,7 +11,7 @@ render(
   html`
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class="icon icon-tabler icon-tabler-edit"
+      class="icon icon-tabler icon-tabler-code"
       width="44"
       height="44"
       viewBox="0 0 24 24"
@@ -22,9 +22,9 @@ render(
       stroke-linejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" />
-      <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
-      <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
-      <line x1="16" y1="5" x2="19" y2="8" />
+      <polyline points="7 8 3 12 7 16" />
+      <polyline points="17 8 21 12 17 16" />
+      <line x1="14" y1="4" x2="10" y2="20" />
     </svg>
 
     <hr />
