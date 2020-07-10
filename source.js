04a38255e12855c25469a5f68b7cdb1db395049e
Melvin <melvincarvalho@gmail.com>
2020-07-10 17:53:13 +0200

    add rows cols and pencil

--- source.js
+++ source.js
@@ -3,6 +3,9 @@ import { h, html, render } from 'https://unpkg.com/spux?module'
 
 var arr
 var id = 'data'
+var desc = JSON.stringify(di[id], null, 2)
+var rows = desc ? desc.split('\n').length : 2
+var cols = 80
 
 render(
   html`
@@ -25,14 +28,14 @@ render(
     </svg>
 
     <hr />
-    <textarea>
-        ${di[id]}
+    <textarea cols="${cols}" rows="${rows}">
+        ${desc}
     </textarea
     >
     <hr />
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class="icon icon-tabler icon-tabler-check"
+      class="icon icon-tabler icon-tabler-pencil"
       width="44"
       height="44"
       viewBox="0 0 24 24"
@@ -43,7 +46,8 @@ render(
       stroke-linejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" />
-      <path d="M5 12l5 5l10 -10" />
+      <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />
+      <line x1="13.5" y1="6.5" x2="17.5" y2="10.5" />
     </svg>
   `,
   document.body
