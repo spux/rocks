86f47f69e23f9c9432565eebe2f17c7ddf404e44
Melvin Carvalho <melvincarvalho@gmail.com>
2020-09-23 18:10:46 +0200

    add lodash unescape

--- freeplaneg.js
+++ freeplaneg.js
@@ -34,7 +34,7 @@ const htmlUnescapes = {
 }
 
 /** Used to match HTML entities and HTML characters. */
-const reEscapedHtml = /&(?:amp|lt|gt|quot|#(0+)?39);/g
+const reEscapedHtml = /&(?:amp|lt|gt|apos|quot|#(0+)?39);/g
 const reHasEscapedHtml = RegExp(reEscapedHtml.source)
 
 function unescape (string) {
