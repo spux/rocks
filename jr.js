8331ff29b15579da207f889c84e0ae5e09363032
Melvin Carvalho <melvincarvalho@gmail.com>
2020-09-29 16:00:30 +0200

    two column jr

--- jr.js
+++ jr.js
@@ -5,26 +5,45 @@ document.head.insertAdjacentHTML(
   `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.21.0/themes/prism.min.css" />`
 )
 
+document.head.insertAdjacentHTML(
+  'beforeend',
+  `<link rel="stylesheet" href="https://unpkg.com/spux-components/css/spux.css />`
+)
+
 var id = 'data'
 
 function renderAll () {
   render(
     html`
-      <h2>${di.data.title}</h2>
-      <p>${di.data.description}</p>
-      <hr />
-      <pre><code class="language-json">${JSON.stringify(
-        di.data,
-        null,
-        2
-      )}</code></pre>
-      <hr />
-      <h3>Links</h3>
-      ${di[id].links.map(i => {
-        return html`
-          <a href="${i.link}">➥</a> | <a href="${i.href}">${i.text}</a> <br />
-        `
-      })}
+      <div class="row">
+        <h2>${di.data.title}</h2>
+      </div>
+      <div class="row">
+        <div class="col 6">
+          <pre><code class="language-json">${JSON.stringify(
+            di.data,
+            null,
+            2
+          )}</code></pre>
+        </div>
+        <div class="col 6">
+          <p>
+            ${di.data.publisher} ${di.data.copyright} ${di.data.lang}
+            ${di.data.date}
+          </p>
+          <p>${di.data.description}</p>
+          <hr />
+          <p>${di.data.text.replace('\n', '<br/>')}</p>
+
+          <h3>Links</h3>
+          ${di[id].links.map(i => {
+            return html`
+              <a href="${i.link}">➥</a> | <a href="${i.href}">${i.text}</a>
+              <br />
+            `
+          })}
+        </div>
+      </div>
     `,
     document.body
   )
