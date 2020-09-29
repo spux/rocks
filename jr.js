d3d9d12a572521b3e28dd6ba70d6ef910f1bce14
Melvin Carvalho <melvincarvalho@gmail.com>
2020-09-29 18:14:09 +0200

    try raw return in jr

--- jr.js
+++ jr.js
@@ -23,9 +23,9 @@ function renderAll () {
           <p>${di.data.description}</p>
           <h3>Links</h3>
           ${di[id].links.map(i => {
-            return html`
+            return `
               <a href="${i.link}">➥</a> |
-              <a href="${i.href}">${i.text.substring(0, 50)}</a>
+              <a href="${i.href}">${i.text}</a>
               <br />
             `
           })}
